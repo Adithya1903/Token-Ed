911110df7f31b3f29353612c7008fb4cd8708c29
ajay <ajay714@yahoo.com>
2022-11-05 22:12:50 -0700

    admin page

--- tokenised-education/pages/organizations/my-organizations/admin/[index].js
+++ tokenised-education/pages/organizations/my-organizations/admin/[index].js
@@ -1,32 +1,21 @@
-import { getSession, signOut } from "next-auth/react";
+import { getSession } from "next-auth/react";
 import connectDB from "../../../../lib/connectDB";
 import Users from "../../../../lib/userSchema";
-import Students from "../../../../lib/studentSchema";
-import { useRouter } from "next/router";
+import Students from "../../../../lib/studentSchema"
 import Professors from "../../../../lib/professorSchema";
+import { useRouter } from "next/router";
 
+export default function (props) {
 
-
-
-
-export default async function (props) {
-  const router = useRouter();
-
+const router = useRouter();
   function addOrg() {
     router.push("/organizations/create-an-organization")
   }
 
-  if (props.user.accountType == "student") {
-
-
-   const user = await Users.findOne({profileId : props.user.profileId});
-
-    const student = await Students.findOne({user:user})
-
-    const groups = student.groups_owned
+    const groups = props.groups
 
     return (
-      <div>
+        <div>
         <ul>
         {groups.map((group) => (
           <li>{group}</li>
@@ -35,55 +24,54 @@ export default async function (props) {
         <button onClick={addOrg}>add organizations</button>
       </div>
     );
-  }
-  if (props.user.accountType == "professor") {
-
-    console.log("here");
-
-    const user = await Users.findOne({ profileId: props.user.profileId });
-
-    const professor = await Professors.findOne({user: user})
-
-    console.log(professor)
-    
-    
-        return (
-            <>
-            <div>
-            <button onClick={addOrg}>add organizations</button>
-            </div>
-            </>
-        )
-
-
-  }
-  if (props.user.accountType == "admin") {
-    return <h1>admin dashboard</h1>;
-  }
+        
 }
 
-export async function getServerSideProps(context) {
-  /* get user session */
-  const session = await getSession(context);
-  console.log(session);
 
-  if (!session) {
+
+
+export async function getServerSideProps(context) {
+    /* get user session */
+    const session = await getSession(context);
+    console.log(session);
+  
+    if (!session) {
+      return {
+        redirect: {
+          destination: "/signin",
+          permanent: false,
+        },
+      };
+    }
+  
+    /* connect to mongoDB, find user and check if they need to register account details */
+    await connectDB();
+    const user = await Users.findOne({ profileId: session.user.profileId });
+
+    var groups;
+
+    if (user.accountType == "student") {
+        const student = await Students.findOne({user: user})
+        console.log(student);
+
+        groups = student.groups_owned;
+    }
+    else if (user.accountType == "professor") {
+        const professor = await Professors.findOne({user: user})
+        console.log(professor);
+
+        groups = professor.groups;
+    }
+    if (user.accountType == "admin") {
+
+        groups = [];
+    }
+  
+
     return {
-      redirect: {
-        destination: "/signin",
-        permanent: false,
+      props: {
+        user: JSON.parse(JSON.stringify(user)),
+        groups: JSON.parse(JSON.stringify(groups)),
       },
     };
   }
-
-  /* connect to mongoDB, find user and check if they need to register account details */
-  await connectDB();
-  const user = await Users.findOne({ profileId: session.user.profileId });
-  console.log(user.accountType);
-
-  return {
-    props: {
-      user: JSON.parse(JSON.stringify(user)),
-    },
-  };
-}
